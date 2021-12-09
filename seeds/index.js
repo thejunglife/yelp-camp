8992e84b3d75bf4a2c211b2fc5a8adef528bf6de
Paul <thejunglife@gmail.com>
2021-12-08 17:43:14 -0800

    added mapbox feature

--- seeds/index.js
+++ seeds/index.js
@@ -23,10 +23,12 @@ const seedDB = async() => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      //Your user id
       author: '619dc4769c7ce9e0209f281a',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       price,
+      geometry: { type: 'Point', coordinates: [ -113.1331, 47.0202 ] },
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus facere minima praesentium a asperiores, at sed sint quasi adipisci veniam id quibusdam expedita sequi eum, sapiente eveniet! Accusantium, nisi autem.',
       images: [
     {

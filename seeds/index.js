c222e6733e25f61381252a57a3f106e5c007e7ad
Paul <thejunglife@gmail.com>
2021-12-09 16:48:51 -0800

    added cluster map feature

--- seeds/index.js
+++ seeds/index.js
@@ -19,7 +19,7 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async() => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 300; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
@@ -28,7 +28,11 @@ const seedDB = async() => {
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       price,
-      geometry: { type: 'Point', coordinates: [ -113.1331, 47.0202 ] },
+      geometry: { type: 'Point', 
+      coordinates: [ 
+        cities[random1000].longitude,
+        cities[random1000].latitude
+       ] },
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus facere minima praesentium a asperiores, at sed sint quasi adipisci veniam id quibusdam expedita sequi eum, sapiente eveniet! Accusantium, nisi autem.',
       images: [
     {
